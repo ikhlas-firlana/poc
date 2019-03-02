544f17bda26bbb1419e8ac4907be3c94aa1d1e16
develop <ikhlas.firlana@gmail.com>
2019-03-03 01:40:01 +0700

    no message

---
+++ pupeteerjs-big-bad-wolf/index.js
@@ -0,0 +1,17 @@
+/**
+ * My User stories
+ * this only stories to populate big bad wolf book or index to json
+ * samples, https://www.bigbadwolfbooks.com/id/books/family-and-relationships/1
+ * using this url https://www.bigbadwolfbooks.com/id/books/{:slug}/{:page}
+ * :slug get from categories (xpath: /html/body/div[1]/div/main/div[3]/div[1]/nav)
+ * :page get from pagination (xpath: /html/body/div[1]/div/main/div[3]/div[4]/section/div[2])
+ * populate schema json
+ * 
+ * - categories
+ *      img
+ *      title
+ *      price
+ *      value
+ *
+ * Get To Works!
+ */
